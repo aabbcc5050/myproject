7127076c2154b6540940e52ec5b2577b5bc66fad
aabbcc5050 <aabbcc5050@qq.com>
2018-02-20 22:25:25 +0800

    test

---
+++ hicharts_learn/src/main/java/com/example/demo/HichartsLearnApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HichartsLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HichartsLearnApplication.class, args);
+	}
+}
